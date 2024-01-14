dfb6172f9c1140cab89ef36533c347acce24f8fe
jede <jede@oric.org>
2024-01-14 23:24:22 +0100

    fix

--- src/include/ch376.h
+++ src/include/ch376.h
@@ -221,7 +221,7 @@ void ch376_reset_all(void);
 unsigned char ch376_wait_response(void);
 void ch376_set_usb_speed(unsigned char speed);
 void ch376_set_usb_addr(unsigned char adress);
-void ch376_set_addr(unsigned char adress);
+void ch376_set_address(unsigned char adress);
 void ch376_get_descr(unsigned char mode);
 void ch376_set_config(unsigned char config);
 
