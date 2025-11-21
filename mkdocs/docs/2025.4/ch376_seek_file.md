87f0e48fa25e4f96ce6affe8b355f838ae365c28
jede <jede@oric.org>
2025-11-21 23:06:42 +0100

    fix/doc

---
+++ mkdocs/docs/2025.4/ch376_seek_file.md
@@ -0,0 +1,33 @@
+---
+
+## ch376_seek_file
+
+***Description***
+
+Seek file. Performs a wait_response
+
+***Input***
+
+* Accumulator : First byte
+* Y Register : second byte
+* X Register : third byte byte
+* RES : Fourth byte
+
+***Modify***
+
+* Accumulator * X Register from ch376_wait_response call* Y Register from ch376_wait_response call
+***Returns***
+
+* Accumulator : ch376 status values
+
+ lda #$04
+ sta RES
+ lda #$01
+ ldy #$02
+ ldx #$03
+ jsr ch376_seek_file
+ ; check accumulator here ch376_wait_response had been launched by ch376_seek_file
+```
+
+!!! note "Not tested"
+
---
+++ src/ch376_seek_file.s
@@ -0,0 +1,41 @@
+
+
+.include "telestrat.inc"
+.include "include/ch376.inc"
+
+
+.export ch376_seek_file
+
+
+; [IN] AY : ptr
+.proc ch376_seek_file
+    ;;@brief Seek file. Performs a wait_response
+    ;;@inputA First byte
+    ;;@inputY second byte
+    ;;@inputX third byte byte
+    ;;@inputMEM_RES Fourth byte
+    ;;@modifyA
+    ;;@modifyX from ch376_wait_response call
+    ;;@modifyY from ch376_wait_response call
+    ;;@returnsA ch376 status values
+    ;;@```ca65
+    ;;@`  lda       #$04
+    ;;@`  sta       RES
+    ;;@`  lda       #$01
+    ;;@`  ldy       #$02
+    ;;@`  ldx       #$03
+    ;;@`  jsr       ch376_seek_file
+    ;;@`  ; check accumulator here ch376_wait_response had been launched by ch376_seek_file
+    ;;@```
+    ;;@note Not tested
+    pha
+    lda     #CH376_BYTE_LOCATE
+    sta     CH376_COMMAND
+    pla
+    sta     CH376_DATA
+    sty     CH376_DATA
+    stx     CH376_DATA
+    lda     RES
+    sta     CH376_DATA
+    jmp     ch376_wait_response
+.endproc
